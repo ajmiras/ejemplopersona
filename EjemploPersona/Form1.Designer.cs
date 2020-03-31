b1bfed807fe93d603aa43987ce1b6284a214274f
Antonio Javier Miras Llamas <ajmiras@gmail.com>
2020-03-31 09:10:32 +0200

    Agregar archivos de proyecto.

---
+++ EjemploPersona/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace EjemploPersona
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(192, 85);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 0;
+            this.txtAltura.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtAltura_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAltura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAltura;
+    }
+}
+
---
+++ EjemploPersona/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploPersona
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Persona jose = new Persona();
+            Persona javier = new Persona();
+
+            jose.SetAltura(-1.75);
+
+            javier.SetAltura(2.00);
+
+            txtAltura.Text = Convert.ToString(jose.GetAltura());
+            
+            
+        }
+
+        private void txtAltura_MouseClick(object sender, MouseEventArgs e)
+        {
+            //
+        }
+    }
+
+
+    public class Persona
+    {
+        private double altura = 0.1;
+
+        private bool AlturaCta(double alt)
+        {
+            if (alt > 0.1)
+                return true;
+
+            return false;
+        }
+
+        public double GetAltura()
+        {
+            return altura;
+        }
+
+        public void SetAltura(double alt)
+        {
+            if (AlturaCta(alt))
+                altura = alt;
+        }
+
+
+    }
+
+}
